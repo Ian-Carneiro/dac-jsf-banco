b004ae49ee0485b5ee458779663624ebd0881114
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2019-05-18 18:59:03 -0300

    Criação da classe ConnectionFactory

---
+++ src/main/java/br/edu/ifpb/jdbc/ConnectionFactory.java
@@ -0,0 +1,50 @@
+package br.edu.ifpb.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+	private static String user;
+	private static String password;
+	private static String url;
+	private static String driver;
+	private static ConnectionFactory instance = null;
+	private static Connection connection = null;
+	private ConnectionFactory() {}
+	public static ConnectionFactory getInstance() {
+		if(instance == null) {
+			synchronized (ConnectionFactory.class) {
+				if(instance==null) {
+					instance = new ConnectionFactory();
+				}
+			}
+		}
+		return instance;
+	}
+	static {
+		Properties properties = new Properties();
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties");
+		try {
+			properties.load(inputStream);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		url = properties.getProperty("database.url");
+		user = properties.getProperty("database.user");
+		password = properties.getProperty("database.password");
+		driver = properties.getProperty("database.driver");
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+		}catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public Connection getConnection() {
+		return connection;
+	}
+}
