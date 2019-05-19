8223e8305858f0cbf3ef1fb32c09ffc5e625122d
camilacarwalho <camilacarwalho3@gmail.com>
2019-05-19 14:27:14 -0300

    Bean para integrante

--- src/main/java/br/edu/ifpb/web/jsf/IntegranteBean.java
+++ src/main/java/br/edu/ifpb/web/jsf/IntegranteBean.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.util.List;
 
 @SessionScoped
@@ -31,4 +32,46 @@ public class IntegranteBean implements Serializable {
     public Integrante getIntegrante() {
         return integrante;
     }
+    public void setIntegrante(Integrante integrante) {
+        this.integrante = integrante;
+    }
+
+    public String salvar() {
+        try {
+            this.integranteDao.cadastrar(integrante);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        this.integrante = new Integrante();
+        return "index.xhtml?faces-redirect=true";
+    }
+    public List<Integrante> listarTodosOsIntegrantes(){
+        try {
+            return this.integranteDao.listar();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String atualizar() {
+        try {
+            this.integranteDao.atualizar(integrante);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        this.integrante = new Integrante();
+        return null;
+    }
+
+    public String excluir(Integrante integrante) {
+        try {
+            this.integranteDao.excluir(integrante);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
 }
