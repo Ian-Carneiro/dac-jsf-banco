09b52c441a0037ddd5d3ad9a2836a7ca4038497a
MailsonD <mailssondennis@gmail.com>
2019-05-19 17:56:42 -0300

    Scripts de criação e população do banco

---
+++ Database/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO integrante (nome,dataDeNascimento,CPF) VALUES('Lucas','1998-10-15','111.111.111-11');
+INSERT INTO integrante (nome,dataDeNascimento,CPF) VALUES('Jonas','2000-07-05','222.222.222-22');
+INSERT INTO integrante (nome,dataDeNascimento,CPF) VALUES('Marcos','1910-03-23','333.333.333-33');
+INSERT INTO integrante (nome,dataDeNascimento,CPF) VALUES('Irineu','1973-11-01','444.444.444-44');
+INSERT INTO integrante (nome,dataDeNascimento,CPF) VALUES('Jaum','2005-04-27','555.555.555-55');
+
+INSERT INTO banda (localDeOrigem,nomeFantasia) VALUES('Narnia','Meu pagodinho');
+INSERT INTO banda (localDeOrigem,nomeFantasia) VALUES('Disney','Forrozinho da budega');
+
+INSERT INTO integrante_banda (id_integrante,id_banda) VALUES(1,1);
+INSERT INTO integrante_banda (id_integrante,id_banda) VALUES(2,1);
+INSERT INTO integrante_banda (id_integrante,id_banda) VALUES(3,1);
+INSERT INTO integrante_banda (id_integrante,id_banda) VALUES(4,2);
+INSERT INTO integrante_banda (id_integrante,id_banda) VALUES(5,2);
+
