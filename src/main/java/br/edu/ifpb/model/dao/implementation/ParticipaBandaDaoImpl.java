d07ec10e6d1aa0e2b3ae7a6fdd9eaafe397e6ee6
MailsonD <mailssondennis@gmail.com>
2019-05-18 22:13:45 -0300

    Funcinalidades para buscar buscar banda com participantes

--- src/main/java/br/edu/ifpb/model/dao/implementation/ParticipaBandaDaoImpl.java
+++ src/main/java/br/edu/ifpb/model/dao/implementation/ParticipaBandaDaoImpl.java
@@ -1,4 +1,36 @@
 package br.edu.ifpb.model.dao.implementation;
 
-public class ParticipaBandaDaoImpl {
+import br.edu.ifpb.model.dao.intefaces.BandaDao;
+import br.edu.ifpb.model.dao.intefaces.IntegranteDao;
+import br.edu.ifpb.model.dao.intefaces.ParticipaBandaDao;
+import br.edu.ifpb.model.domain.Banda;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ParticipaBandaDaoImpl implements ParticipaBandaDao {
+
+    private BandaDao bandaDao;
+    private IntegranteDao integranteDao;
+
+    public ParticipaBandaDaoImpl() {
+        bandaDao = new BandaDaoImpl();
+        integranteDao = new IntegranteDaoImpl();
+    }
+
+    @Override
+    public Banda buscarBandaComParticipantes(String localDeOrgiem) throws SQLException {
+        Banda banda = bandaDao.buscarPorLocalDeOrigem(localDeOrgiem);
+        banda.setIntegrantes(integranteDao.buscarPorBanda(banda.getId()));
+        return banda;
+    }
+
+    @Override
+    public List<Banda> listarBandasComParticipantes() throws SQLException {
+        List<Banda> bandas = bandaDao.listar();
+        for (Banda banda:bandas){
+            banda.setIntegrantes(integranteDao.buscarPorBanda(banda.getId()));
+        }
+        return bandas;
+    }
 }
--- src/main/java/br/edu/ifpb/model/dao/intefaces/ParticipaBandaDao.java
+++ src/main/java/br/edu/ifpb/model/dao/intefaces/ParticipaBandaDao.java
@@ -3,9 +3,11 @@ package br.edu.ifpb.model.dao.intefaces;
 import br.edu.ifpb.model.domain.Banda;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface ParticipaBandaDao {
 
-    Banda buscarBandaComParticipantes(int idBanda) throws SQLException;
+    Banda buscarBandaComParticipantes(String localDeOrigem) throws SQLException;
+    List<Banda> listarBandasComParticipantes() throws SQLException;
 
 }
